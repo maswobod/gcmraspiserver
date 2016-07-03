94378d5eb992b4aa48cc46b22b24259a80ae2677
maswobod <martin@martinswoboda.de>
2016-07-03 15:20:38 +0200

    try something new

--- services/button.js
+++ services/button.js
@@ -19,10 +19,9 @@ util.inherits(button, EventEmitter);
 
 button.prototype.init = function( btn_port ){
 	port = btn_port;
-    rpio.open(port, rpio.INPUT, rpio.PULL_DOWN);
+    rpio.open(port, rpio.INPUT);
     messageSend.init('AIzaSyBAirrWt0-MbnVqR5l8YTIsc0foFYmHJPc');
 
-    rpio.poll(pin, pollcb, rpio.POLL_HIGH);
 	BTN();
 };
 
@@ -31,34 +30,16 @@ button.prototype.init = function( btn_port ){
  */ 
 
 button.prototype.checkIfPressed = function(){
-    console.log('Pin '+ port + ' is currently set ' + (rpio.read(port) ? 'high' : 'low'));
+    gpio.read(port, function(err, value) {
+    if(err) throw err;
+    console.log(value); // The current state of the pin
+});
+
 };
 
 function BTN(){
 	console.log("Button defined");
 };
-
-function pollcb(cbpin)
-{
-    /*
-     * It cannot be guaranteed that the current value of the pin is the
-     * same that triggered the event, so the best we can do is notify the
-     * user that an event happened and print what the value is currently
-     * set to.
-     *
-     * Unless you are pressing the button faster than 1ms (the default
-     * setInterval() loop which polls for events) this shouldn't be a
-     * problem.
-     */
-    var state = rpio.read(cbpin) ? 'pressed' : 'released';
-    console.log('Button event on P%d (button currently %s)', cbpin, state);
-
-    /*
-     * By default this program will run forever.  If you want to cancel the
-     * poll after the first event and end the program, uncomment this line.
-     */
-    // rpio.poll(cbpin, null);
-}
 
 module.exports = new button;
 
