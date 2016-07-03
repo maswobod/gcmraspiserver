49d8c4cae84d5e6be6b232e97e47d41471ae9265
maswobod <martin@martinswoboda.de>
2016-07-03 15:48:23 +0200

    sight

--- testBtn.js
+++ testBtn.js
@@ -1,6 +1,12 @@
 var Gpio = require('onoff').Gpio,
-  button = new Gpio(6, 'in', 'both');
+  led = new Gpio(4, 'out'),
+  button = new Gpio(5, 'in', 'both');
 
- button.watch(function(err, value) {
-  console.log("SOmething happend");
+button.watch(function(err, value) {
+  led.writeSync(value);
+});
+
+process.on('SIGINT', function () {
+  led.unexport();
+  button.unexport();
 });
