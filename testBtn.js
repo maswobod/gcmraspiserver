473110cc27f1810599528f228c705bbd8b144767
maswobod <martin@martinswoboda.de>
2016-07-03 15:50:43 +0200

    i think now

--- testBtn.js
+++ testBtn.js
@@ -1,12 +1,10 @@
 var Gpio = require('onoff').Gpio,
-  led = new Gpio(4, 'out'),
   button = new Gpio(5, 'in', 'both');
 
 button.watch(function(err, value) {
-  led.writeSync(value);
+  console.log(value); 
 });
 
 process.on('SIGINT', function () {
-  led.unexport();
   button.unexport();
 });
