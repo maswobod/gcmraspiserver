f818833f6acc3069cecfc8a3ec12377f1135b560
maswobod <martin@martinswoboda.de>
2016-07-03 15:38:42 +0200

    try something really new

--- testBtn.js
+++ testBtn.js
@@ -1,5 +1,5 @@
 var Gpio = require('onoff').Gpio,
-  button = new Gpio(5, 'in', 'both');
+  button = new Gpio(6, 'in', 'both');
 
  button.watch(function(err, value) {
   console.log("SOmething happend");
